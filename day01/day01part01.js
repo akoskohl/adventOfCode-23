0a18dd9f29983c80314b60066b0038aa9be4f050
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-03-04 17:23:33 +0100

    last digit

--- day01/day01part01.js
+++ day01/day01part01.js
@@ -17,8 +17,16 @@ function calibrateAndSumValues (input) {
     summerizedValues = 0;
     for (let i = 0; i < input.length; i++) {
         firstDigit = getFirstIntegerFromString(input[i]);
+        lastDigit = getLastIntegerFromString(input[i]);
     }
+function reverseString (string) {
+    return string.split("").reverse().join("");
 }
+
 function getFirstIntegerFromString (string) {
     return String(string.match(/[0-9]+/)).charAt(0);
+}
+
+function getLastIntegerFromString (string) {
+    return String(reverseString(string).match(/[0-9]+/)).charAt(0);
 }
