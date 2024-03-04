5062ff3640d97b3bb0a318cb275eeb4bd3db5cc5
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-03-04 17:20:41 +0100

    inputfile and readfile

--- day01/day01part01.js
+++ day01/day01part01.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
+
+day = '01'
+inputFile = 'day' + day + 'part' + day + '-input.txt';
+// inputFile = 'day01part1-test.txt';
+input = [];
 solution();
 
 function solution() {
+    readInputfile(inputFile);
 function readInputfile (inputFile) {
     input = fs.readFileSync(inputFile, "utf-8").split("\n");
 }
