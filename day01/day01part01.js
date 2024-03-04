7d07790135318ac147e3fd129cf146bf0234e1b0
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-03-04 17:24:14 +0100

    append int to sum, finalizing

--- day01/day01part01.js
+++ day01/day01part01.js
@@ -4,21 +4,34 @@ day = '01'
 inputFile = 'day' + day + 'part' + day + '-input.txt';
 // inputFile = 'day01part1-test.txt';
 input = [];
+
+calibratedValues = [];
+
 solution();
 
 function solution() {
     readInputfile(inputFile);
+    console.log(calibrateAndSumValues(input));
 }
+
 function readInputfile (inputFile) {
     input = fs.readFileSync(inputFile, "utf-8").split("\n");
 }
 
 function calibrateAndSumValues (input) {
     summerizedValues = 0;
+
     for (let i = 0; i < input.length; i++) {
         firstDigit = getFirstIntegerFromString(input[i]);
         lastDigit = getLastIntegerFromString(input[i]);
+        calibratedValue = firstDigit + '' + lastDigit;
+        summerizedValues += parseInt(calibratedValue);
+        // console.log(input[i], calibratedValue);
     }
+
+    return summerizedValues;
+}
+
 function reverseString (string) {
     return string.split("").reverse().join("");
 }
