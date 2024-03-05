e75d27cea873ce3db620703d4ac3b1e89d9e9ea5
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-03-05 18:01:05 +0100

    main function

--- day01/day01-part1.go
+++ day01/day01-part1.go
@@ -9,6 +9,12 @@ import (
 var filePath = "day01part01-input.txt"
 // var filePath = "day01part1-test.txt"
 
+func main() {
+    content, err := readInputFile(filePath)
+    if err != nil {
+        fmt.Println(err)
+    }
+
 func readInputFile(filePath string) (string, error) {
     content, err := os.ReadFile(filePath)
     if err != nil {
