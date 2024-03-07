6adee7981c72495e52dc0e0b0aa0ac7335addb06
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-03-07 20:28:28 +0100

    main function and file read

---
+++ day01/day01part02.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+calibratedValues = [];
+
+solution();
+
+function solution() {
+function readInputfile (inputFile) {
+    input = fs.readFileSync(inputFile, "utf-8").split("\n");
+}
+
+}
