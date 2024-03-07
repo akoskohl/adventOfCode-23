30c818cf0dec3a8514ff0413fba2580ce94d4be1
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-03-07 20:29:01 +0100

    read file

--- day01/day01part02.js
+++ day01/day01part02.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 
+day = '01'
+inputFile = 'day' + day + 'part' + day + '-input.txt';
+input = [];
 calibratedValues = [];
 
 solution();
 
 function solution() {
+    readInputfile(inputFile);
 function readInputfile (inputFile) {
     input = fs.readFileSync(inputFile, "utf-8").split("\n");
 }
